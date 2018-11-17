c1514064c726f8dfa0b64383fe2cd076e152949d
Kanuka <kanukamisaki@icloud.com>
2018-11-17 16:46:10 +0900

    リファクタリング：ガード節を採用した

--- fizzbuzz-2.py
+++ fizzbuzz-2.py
@@ -1,13 +1,14 @@
 def fizzbuzz_convert(number):
     if number % 15 == 0:
         return "FizzBuzz"
-    elif number % 3 == 0:
-        return "Fizz"
-    elif number % 5 == 0:
-        return "Buzz"
-    else:
-        return str(number)
 
+    if number % 3 == 0:
+        return "Fizz"
+
+    if number % 5 == 0:
+        return "Buzz"
+
+        return str(number)
 
 result = fizzbuzz_convert(1)
 print(result)  # 1
